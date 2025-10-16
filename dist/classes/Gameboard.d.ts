627a7ba5e50b6a4ec9413592a737083adb89941d
robkroening <rob.kroening@gmail.com>
2025-10-16 16:20:30 -0400

    added classes folder and created gameboard class. also edited types in tsconfig and package.json

---
+++ dist/classes/Gameboard.d.ts
@@ -0,0 +1,8 @@
+declare class Gameboard {
+    length: number;
+    width: number;
+    constructor(length: number, width: number);
+    greet(): void;
+}
+export default Gameboard;
+//# sourceMappingURL=Gameboard.d.ts.map
---
+++ dist/classes/Gameboard.js
@@ -0,0 +1,16 @@
+// this will be the class for the gameboard.
+// here this will organize everything about the gameboard and what it can do.
+// properties -> constructor -> methods
+class Gameboard {
+    // constructor
+    constructor(length, width) {
+        this.length = length;
+        this.width = width;
+    }
+    // methods
+    greet() {
+        console.log("I am literally a gameboard saying hello. dont make no sense boy");
+    }
+}
+export default Gameboard;
+//# sourceMappingURL=Gameboard.js.map
