6203d35ced80a446406f0d9627d772d50af579f5
robkroening <rob.kroening@gmail.com>
2025-10-17 19:54:51 -0400

    added the gameboard class and draw a blank board function

--- src/classes/Gameboard.ts
+++ src/classes/Gameboard.ts
@@ -5,18 +5,27 @@
 // properties -> constructor -> methods
 class Gameboard {
     // properties
-    length: number;
-    width: number;
+    length: number = 10;
+    width: number = 10;
     
     // constructor
-    constructor(length: number, width: number) {
-        this.length = length;
-        this.width = width;
-    }
+    constructor() {}
 
     // methods
-    greet(): void {
-        console.log("I am literally a gameboard saying hello. dont make no sense boy")
+    drawBlankBoard(): void {
+        // tell the user the blank board is being drawn
+        console.log(`Drawing a blank board that is ${this.length}x${this.width}`);
+
+        // draw an empty 10x10 board
+        const array: any = [];
+        for (let i: number = 0; i < 10; i++) {
+            array[i] = [];
+            // now established rows, time to do columns
+            for (let j: number = 0; j < 10; j++) {
+                array[i][j] = `row${i+1} column${j+1}`;
+            }
+        }
+        console.log(array);
     }
 }
 
